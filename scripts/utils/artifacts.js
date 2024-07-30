b197542e46c0ed29c1385cd1261c7a67235db31d
Le Hoang Anh <anh.le@angelhack.com>
2024-07-30 11:19:00 +0700

    Add error handling to artifacts.js

--- scripts/utils/artifacts.js
+++ scripts/utils/artifacts.js
@@ -3,20 +3,24 @@ const { Constants, Deployer, DeploymentStorage } = require("./env");
 
 const Contracts = {};
 const ABIs = {};
-const DeploymentInfo = DeploymentStorage.Env[Constants.ENV_KEY]
+const DeploymentInfo = DeploymentStorage.Env[Constants.ENV_KEY];
 const contractNames = DeploymentInfo ? Object.keys(DeploymentInfo) : [];
 for (const contractName of contractNames) {
-    const contractABI = require(`../../ABIs/${contractName}_ABI.json`);
-    const contractAddress =
-        DeploymentStorage.Env[Constants.ENV_KEY][contractName].Proxy ||
-        DeploymentStorage.Env[Constants.ENV_KEY][contractName].Beacon;
-    const contract = new ethers.Contract(
-        contractAddress,
-        contractABI,
-        Deployer,
-    );
-    ABIs[contractName] = contractABI;
-    Contracts[contractName] = contract;
+    try {
+        const contractABI = require(`../../ABIs/${contractName}_ABI.json`);
+        const contractAddress =
+            DeploymentStorage.Env[Constants.ENV_KEY][contractName].Proxy ||
+            DeploymentStorage.Env[Constants.ENV_KEY][contractName].Beacon;
+        const contract = new ethers.Contract(
+            contractAddress,
+            contractABI,
+            Deployer,
+        );
+        ABIs[contractName] = contractABI;
+        Contracts[contractName] = contract;
+    } catch (err) {
+        continue;
+    }
 }
 
 module.exports = { Contracts, ABIs };
