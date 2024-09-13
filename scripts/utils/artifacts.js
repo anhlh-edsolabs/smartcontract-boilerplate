a7d05eaff0bf98e8b1c275759f40c4d8d932f840
Le Hoang Anh <anh.le@angelhack.com>
2024-09-13 11:40:55 +0700

    code: refactor artifact.js

--- scripts/utils/artifacts.js
+++ scripts/utils/artifacts.js
@@ -1,23 +1,33 @@
 const { ethers } = require("ethers");
 const { Constants, Deployer, DeploymentStorage } = require("./env");
+const { Utils } = require("./utils");
 
-const Contracts = {};
-const ABIs = {};
+const Contracts = {}, ABIs = {};
 const DeploymentInfo = DeploymentStorage.Env[Constants.ENV_KEY];
 const contractNames = DeploymentInfo ? Object.keys(DeploymentInfo) : [];
 for (const contractName of contractNames) {
     try {
-        const contractABI = require(`../../ABIs/${contractName}_ABI.json`);
+        const { artifactName, deploymentName } =
+            Utils.getContractName(contractName);
+
+        const contractABI = require(`../../ABIs/${artifactName}_ABI.json`);
+        // assign contractAddress to DeploymentInfo[contractName].Proxy
+        // or DeploymentInfo[contractName].Beacon if they exist,
+        // otherwise assign it to DeploymentInfo[contractName].Impl
         const contractAddress =
-            DeploymentStorage.Env[Constants.ENV_KEY][contractName].Proxy ||
-            DeploymentStorage.Env[Constants.ENV_KEY][contractName].Beacon;
+            DeploymentInfo[contractName].Proxy ||
+            DeploymentInfo[contractName].Beacon ||
+            DeploymentInfo[contractName].Impl;
+
         const contract = new ethers.Contract(
             contractAddress,
             contractABI,
             Deployer,
         );
-        ABIs[contractName] = contractABI;
-        Contracts[contractName] = contract;
+        const contractAlias =
+            deploymentName != "" ? deploymentName : artifactName;
+        ABIs[contractAlias] = contractABI;
+        Contracts[contractAlias] = contract;
     } catch (err) {
         continue;
     }
