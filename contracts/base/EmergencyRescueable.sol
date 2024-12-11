47db947bd1e8ba86d1362b4339fdf4c5a70fba6e
Le Hoang Anh <anh.le@angelhack.com>
2024-12-11 14:35:16 +0700

    code: add EmergencyRescueable contract

---
+++ contracts/base/EmergencyRescueable.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {Validation} from "../libs/Utils.sol";
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+abstract contract EmergencyRescueable {
+    using SafeERC20 for IERC20;
+
+    error NativeCoinIsNotSupported();
+
+    event FundRescued(
+        address token,
+        address recipient,
+        uint256 amount,
+        uint256 timestamp
+    );
+
+    function rescue(
+        address token,
+        address recipient,
+        uint256 amount
+    ) external virtual {
+        Validation.noZeroAddress(token);
+        Validation.noZeroAddress(recipient);
+        Validation.noZeroAmount(amount);
+
+        amount = amount == type(uint256).max
+            ? IERC20(token).balanceOf(address(this))
+            : amount;
+
+        IERC20(token).safeTransfer(recipient, amount);
+
+        emit FundRescued(token, recipient, amount, block.timestamp);
+    }
+
+    receive() external virtual payable {
+        _fallback();
+    }
+
+    fallback() external virtual payable {
+        _fallback();
+    }
+
+    function _fallback() internal pure {
+        revert NativeCoinIsNotSupported();
+    }
+}
